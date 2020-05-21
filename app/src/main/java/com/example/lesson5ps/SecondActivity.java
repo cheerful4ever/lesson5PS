7753a940ab91648741308bb408e5c8bd7cd6d90d
18016375 <18016375@myrp.edu.sg>
2020-05-21 22:43:01 +0800

    activities 1-10000000000

--- app/src/main/java/com/example/lesson5ps/SecondActivity.java
+++ app/src/main/java/com/example/lesson5ps/SecondActivity.java
@@ -3,7 +3,10 @@ package com.example.lesson5ps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -11,6 +14,7 @@ public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     SongArrayAdapter adapter;
+    Button btnShowStars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,13 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         lv = (ListView) this.findViewById(R.id.lv);
+        btnShowStars = (Button) this.findViewById(R.id.btnShow5Stars);
 
         DBHelper dbh = new DBHelper(this);
         ArrayList<Song> songs = dbh.getAllSongs();
 
         adapter = new SongArrayAdapter(this, R.layout.row, songs);
         lv.setAdapter(adapter);
+        
     }
 }
