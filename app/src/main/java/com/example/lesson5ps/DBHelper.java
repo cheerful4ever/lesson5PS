8f5cc979f316b62f0f09810dd776eb3e4fe584bb
18016375 <18016375@myrp.edu.sg>
2020-05-21 14:49:31 +0800

    activities 1-10000000000

--- app/src/main/java/com/example/lesson5ps/DBHelper.java
+++ app/src/main/java/com/example/lesson5ps/DBHelper.java
@@ -1,9 +1,13 @@
 package com.example.lesson5ps;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -16,27 +20,119 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String COLUMN_YEAR = "year";
     private static final String COLUMN_STARS = "stars";
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String createNoteTableSql = "CREATE TABLE " + TABLE_SONG + "("
-                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_TITLE + " TEXT " + COLUMN_SINGER + " TEXT " + COLUMN_YEAR + " INTEGER " + COLUMN_STARS + " INTEGER ) ";
-        db.execSQL(createNoteTableSql);
-        Log.i("info", "created tables");
-
-        //Dummy records, to be inserted when the database is created
-        for (int i = 0; i< 4; i++) {
-            ContentValues values = new ContentValues();
-            values.put(COLUMN_NOTE_CONTENT, "Data number " + i);
-            db.insert(TABLE_SONG, null, values);
-        }
-        Log.i("info", "dummy records inserted");
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
-        db.execSQL("ALTER TABLE " + TABLE_SONG + " ADD COLUMN module_name TEXT ");
-        //onCreate(db);
+    public void onCreate(SQLiteDatabase db) {
+        // CREATE TABLE Note
+        // (id INTEGER PRIMARY KEY AUTOINCREMENT, note_content TEXT, rating
+        // INTEGER );
+        String createNoteTableSql = "CREATE TABLE " + TABLE_SONG + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT, "
+                + COLUMN_SINGER + " TEXT, "
+                + COLUMN_YEAR + " INTEGER, "
+                + COLUMN_STARS + " INTEGER )";
+        db.execSQL(createNoteTableSql);
+        Log.i("info", "created tables");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SONG);
+        onCreate(db);
+    }
+
+    public void insertSong(String title, String singer, int year, int stars) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_SINGER, singer);
+        values.put(COLUMN_YEAR, year);
+        values.put(COLUMN_STARS, stars);
+        db.insert(TABLE_SONG, null, values);
+        db.close();
+    }
+
+    public boolean isExistingSong(String content) {
+        // Select all the notes' content
+        String selectQuery = "SELECT " + COLUMN_NOTE_CONTENT + " FROM "
+                + TABLE_SONG + " WHERE " + COLUMN_NOTE_CONTENT + " = '"
+                + content + "'";
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Run the SQL query and get back the Cursor object
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // moveToFirst() moves to first row
+        if (cursor.moveToFirst()) {
+            return true;
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return false;
+    }
+
+    public ArrayList<Song> getAllSongs() {
+        ArrayList<Song> notes = new ArrayList<Song>();
+        // "SELECT id, note_content, stars FROM note"
+        String selectQuery = "SELECT " + COLUMN_ID + ","
+                + COLUMN_TITLE + ","
+                + COLUMN_SINGER + ","
+                + COLUMN_YEAR + ","
+                + COLUMN_STARS
+                + " FROM " + TABLE_SONG;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // Loop through all rows and add to ArrayList
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String title = cursor.getString(1);
+                String singer = cursor.getString(2);
+                int year = cursor.getInt(3);
+                int stars = cursor.getInt(4);
+
+                Song songs = new Song(id, title, singer, year, stars);
+                notes.add(songs);
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+        return notes;
+    }
+
+    public ArrayList<String> getSongContent() {
+        //TODO return records in Strings
+
+        // Create an ArrayList that holds String objects
+        ArrayList<String> notes = new ArrayList<String>();
+        // Select all the notes' content
+        String selectQuery = "SELECT " + COLUMN_SONG_CONTENT + " FROM "
+                + TABLE_SONG;
+
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Run the SQL query and get back the Cursor object
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // moveToFirst() moves to first row
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row and returns true;
+            // moveToNext() returns false when no more next row to move to
+            do {
+                // Add the note content to the ArrayList object
+                notes.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return notes;
     }
 }
